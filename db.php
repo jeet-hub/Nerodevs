36b6c95c62efb810f12f93ed69eeb4c1ad8cb437
jeet-hub <jeetkumar.pr@gmail.com>
2026-07-14 17:45:49 +0530

    toast removed updated

---
+++ db.php
@@ -0,0 +1,22 @@
+<?php
+
+$host = "localhost";
+$dbname = "YOUR_DATABASE_NAME";
+$username = "YOUR_DATABASE_USER";
+$password = "YOUR_DATABASE_PASSWORD";
+
+try {
+
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $username,
+        $password
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e){
+
+    die("Database Connection Failed");
+
+}
